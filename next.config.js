0de1bcd0fcf23b4000d12511d1fc761e8d8eb9ac
Yakup Kutluksaman <yakupkutluksaman@gmail.com>
2025-07-08 17:35:44 +0300

    next.config.js CommonJS formatına çevrildi, PWA uyumluluğu sağlandı

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import withPWA from 'next-pwa';
+const withPWA = require('next-pwa');
 
 const nextConfig = {
   eslint: {
@@ -14,4 +14,4 @@ const nextConfig = {
   },
 };
 
-export default withPWA(nextConfig);
+module.exports = withPWA(nextConfig);
