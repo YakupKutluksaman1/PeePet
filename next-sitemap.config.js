052fd10067fd8163cfa18c7c68a2ba5aaf395919
Yakup Kutluksaman <yakupkutluksaman@gmail.com>
2025-07-09 15:48:34 +0300

    SEO ve sitemap ayarları güncellendi

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: 'https://peepet.com',
+    siteUrl: 'https://peepet.com.tr',
     generateRobotsTxt: true,
     // Eğer dinamik yollarınız varsa, ek ayarlar eklenebilir
     // Daha gelişmiş yapılandırma için dökümana bakabilirsiniz
