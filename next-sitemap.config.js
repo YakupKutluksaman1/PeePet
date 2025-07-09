59e913dd51e19254c76be0fb2acd08b22587ad6d
Yakup Kutluksaman <yakupkutluksaman@gmail.com>
2025-07-09 15:43:13 +0300

    Sitemap eklendi

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://peepet.com',
+    generateRobotsTxt: true,
+    // Eğer dinamik yollarınız varsa, ek ayarlar eklenebilir
+    // Daha gelişmiş yapılandırma için dökümana bakabilirsiniz
+} 
